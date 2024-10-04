dc2f68c02a17b22877daeec19d6b1899cd4d9e71
sitlle <Vktrt@yandex.ru>
2024-10-04 09:04:35 +0300

    update

--- Backend/main.cpp
+++ Backend/main.cpp
@@ -3,8 +3,14 @@
 #include "Cars/Car.h"
 #include "Cars/Cars.h"
 #include "Process/CreateCars.h"
+#include "Random.h"
 
 int main() {
-    //CreateCars::CreateCar(CarSettings::Direction::Up, CarSettings::Turn::None);
+    for (int i = 0; i < 10; ++i) {
+    CreateCars::CreateRandomCar(CarSettings::Direction::Down, CarSettings::Turn::None,
+                                static_cast<double>(Random::mt() % 360),
+                                static_cast<double>(Random::mt() % 200),
+                                static_cast<double>(Random::mt() % 200));
+    }
     return 0;
 }
--- Backend/src/Cars/Car.cpp
+++ Backend/src/Cars/Car.cpp
@@ -1,15 +1,19 @@
 #include "../../Cars/Car.h"
 #include "../../UserSettings/UserCarSettings.h"
+#include "../../Random.h"
 
 Car::Car(CarSettings::Settings settings, CarSettings::Direction direction,
          CarSettings::Turn turn) {
     car_settings = settings;
     car_direction = direction;
     car_turn = turn;
+    car_settings.speed = static_cast<int32_t>(Random::mt() % UserCarSettings::maximal_speed)
+                         + UserCarSettings::minimal_speed;
 }
 
 Car::Car(CarSettings::Direction direction, CarSettings::Turn turn) {
     car_direction = direction;
     car_turn = turn;
-    car_settings.speed = UserCarSettings::maximal_speed + UserCarSettings::minimal_speed;
+    car_settings.speed = static_cast<int32_t>(Random::mt() % UserCarSettings::maximal_speed)
+            + UserCarSettings::minimal_speed;
 }
